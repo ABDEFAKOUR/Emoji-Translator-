07e4ff82b48689a5c36e0c5e781c45dd75d3f060
Abdessamad Fakour <a.fakour2427@gmail.com>
2026-08-05 10:05:03 +0100

    Create emojis.py

---
+++ emojis.py
@@ -0,0 +1,21 @@
+emoji_dict = {
+    "happy": "😃",
+    "heart": "😍",
+    "rotfl": "🤣",
+    "smile": "😊",
+    "crying": "😭",
+    "kiss": "😘",
+    "clap": "👏",
+    "grin": "😁",
+    "fire": "🔥",
+    "broken": "💔",
+    "think": "🤔",
+    "excited": "🤩",
+    "boring": "🙄",
+    "winking": "😉",
+    "ok": "👌",
+    "hug": "🤗",
+    "cool": "😎",
+    "angry": "😠",
+    "python": "🐍"
+}
